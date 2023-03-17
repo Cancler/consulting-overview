9bdc5224e539aeacd31170d1c7d840dbfe1d5697
ekanzler <ekanzler@ihp-microelectronics.com>
2023-03-17 10:24:23 +0100

    Vue Tutorial FileViewer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mt-2">
+        <h4>{{ path }}</h4>
+        <div class="form-group mt-4 mb-2">
+            <input
+                v-model="search_string"
+                class="form-control form-control-sm"
+                placeholder="File search"
+            />
+        </div>
+        <FilesViewer
+            :files="filtered_files"
+            @back="back"
+            @on_folder_click="open($event.name)"
+        />
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+// ############################################################################
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import {computed, ref} from "vue";
+import {app} from '@electron/remote';
+import fs from 'fs';
+import pathModule from 'path';
+import FilesViewer from "@/components/FilesViewer";
+
+// ############################################################################
+
+const path = ref(app.getAppPath());
+const search_string = ref("");
+
+// ############################################################################
+
+const files = computed(() => {
+    const file_names = fs.readdirSync(path.value);
+
+    return file_names
+        .map(file => {
+            const stats = fs.statSync(pathModule.join(path.value, file));
+
+            return {
+                name: file,
+                size: stats.isFile() ? format_size(stats.size ?? 0) : null,
+                directory: stats.isDirectory()
+            }
+        })
+        .sort((a, b) => {
+            if(a.directory === b.directory) {
+                return a.name.localeCompare(b.name);
+            }
+
+            return a.directory ? -1 : 1
+        })
+});
+const filtered_files = computed(() => {
+    return search_string.value
+        ? files.value.filter(s => s.name.startsWith(search_string.value))
+        : files.value;
+});
+
+// ############################################################################
+
+const format_size = size => {
+    let i = Math.floor(Math.log(size) / Math.log(1024));
+
+    return (
+        (size / Math.pow(1024, i)).toFixed(2) * 1
+        + ' '
+        + ['B', 'kB', 'GB', 'TB'][i]
+    )
+};
+
+const open = folder => {
+    path.value = pathModule.join(path.value, folder);
 }
+
+const back = () => {
+    path.value = pathModule.dirname(path.value);
+}
+
+// ############################################################################
+
+console.log(path);
+console.log(files);
+
 </script>
 
 <style>
---
+++ src/components/FilesViewer.vue
@@ -0,0 +1,71 @@
+<template>
+    <table class="table">
+        <tbody>
+            <tr class="clickable" @click="$emit('back')">
+                <td class="icon-row">
+                    <IconFolderOpen class="icon-folder"/>
+                </td>
+            </tr>
+            <tr
+                v-for="file in files"
+                :key="file.name"
+                :class="{clickable: file.directory}"
+                @click="on_file_click(file)"
+            >
+                <td class="icon-row">
+                    <IconFolder v-if="file.directory" class="icon-folder"/>
+                    <IconFile v-else class="icon-file"/>
+                </td>
+                <td class="text-left">{{ file.name }}</td>
+                <td>
+                    <span class="float-end">{{ file.size }}</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+// ############################################################################
+
+import {defineProps, defineEmits} from 'vue';
+import IconFolder from "@/components/IconFolder";
+import IconFile from "@/components/IconFile";
+import IconFolderOpen from "@/components/IconFolderOpen";
+
+// ############################################################################
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  files: {type: Array, default: () => []}
+});
+const emit = defineEmits(['on_folder_click', 'back'])
+
+// ############################################################################
+
+const on_file_click = file => {
+    if(file.directory){
+        emit('on_folder_click', file);
+    }
+}
+
+// ############################################################################
+</script>
+
+<style>
+.clickable{
+    cursor: pointer;
+}
+.text-left{
+    text-align: left;
+}
+.icon-row {
+  width: 2em;
+}
+.icon-folder {
+  width: 1em;
+}
+.icon-file {
+  width: 0.75em;
+}
+</style>
